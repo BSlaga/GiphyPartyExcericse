d912ca93c1a7bb26c4952dd1f8016d084e9fb097
BSlaga <148803209+BSlaga@users.noreply.github.com>
2024-03-04 16:17:03 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
 console.log("Let's get this party started!");
-let input = document.querySelector('#input');
-let submit = document.querySelector('#submit');
-let remove = document.querySelector('#remove');
-let gallery = document.querySelector('#giphy-gallery')
+const $input = $('#input');
+const $gallery = $('#giphy-gallery')
 
-submit.addEventListener('click',(e)=> {
+
+$("#remove").on('click',(e)=> {
     e.preventDefault();
-    getGiphy(input.value)
+    $gallery.empty();
 })
 
-remove.addEventListener('click',(e)=> {
-    e.preventDefault();
-    gallery.children.remove;
-})
-
-async function getGiphy(giphyType) {
-    let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${giphyType}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
+$("#submit").on('click',async function getGiphy(e) {
+    e.preventDefault()
+    let searchTerm = $input.val();
+    let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
     let imgURL = response.data.data[Math.floor(Math.random()*response.data.data.length)].images.fixed_height.url;
     addGiphy(imgURL);
-}
+})
 
 function addGiphy(url) {
     let newImg = document.createElement('img');
     newImg.src=`${url}`;
-    gallery.append(newImg);
+    $gallery.append(newImg);
 }
